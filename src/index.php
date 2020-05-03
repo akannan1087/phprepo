54a11d0b4207782845c5226fd2d771ae17a4eb64
Devops Coach <kannan.ananth@gmail.com>
2020-05-03 18:33:00 +0000

    index.php edited online with Bitbucket

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 
  <?php echo '<p>Hello guys!!!</p>'; ?> 
- <?php echo '<p>Welcome to PHP Docker App running inside Docker container!!!</ p>'; ?> 
- <?php echo '<p>We have Automated Docker image creation and deployment !!!</ p>'; ?> 
+ <?php echo '<p>Welcome to PHP Docker App running inside Docker container!</ p>'; ?> 
+ <?php echo '<p>We have Automated Docker image creation and deployment.</ p>'; ?> 
  
 
