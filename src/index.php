32433304e479cd826dd99e2157cdd89c1982041b
akannan1087 <15877864+akannan1087@users.noreply.github.com>
2021-02-23 16:54:42 -0600

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,8 +2,4 @@
  <?php echo '<p>Hello Folks, it is possible!!</p>'; ?> 
  <?php echo '<p>Welcome to PHPApp running inside Docker container!</p>'; ?> 
  <?php echo '<p>We have automated Docker image creation and deployment!!!</p>'; ?> 
- <?php echo '<p>We are testing on Ubuntu without installing anything!</p>'; ?> 
  
- 
- 
-
